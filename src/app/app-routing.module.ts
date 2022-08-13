0693eccaddcd158ca4d60d1dba39977c1a28cf28
Sakuja Shamal <sakuja1shamal@gmail.com>
2022-08-13 20:16:44 +0530

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {NewArrivalsPageComponent} from "./components/new-arrivals-page/new-arrivals-page.component";
+import {SizingPageComponent} from "./components/sizing-page/sizing-page.component";
+import {AboutPageComponent} from "./components/about-page/about-page.component";
+import {CustomerSignUpComponent} from "./components/customer-sign-up/customer-sign-up.component";
+import {MakePaymentComponent} from "./components/make-payment/make-payment.component";
+import {CustomerLoginComponent} from "./components/customer-login/customer-login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'customer/home',pathMatch:'full'},
+  {path:'customer/home',component:HomePageComponent},
+  {path:'customer/new-arrivals',component:NewArrivalsPageComponent},
+  {path:'customer/sizing',component:SizingPageComponent},
+  {path:'customer/about',component:AboutPageComponent},
+  {path:'customer/sign-up',component:CustomerSignUpComponent},
+  {path:'customer/login',component:CustomerLoginComponent},
+  {path:'customer/make-payment',component:MakePaymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { CustomerLoginComponent } from './components/customer-login/customer-login.component';
+import { CustomerSignUpComponent } from './components/customer-sign-up/customer-sign-up.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MakePaymentComponent } from './components/make-payment/make-payment.component';
+import { NewArrivalsPageComponent } from './components/new-arrivals-page/new-arrivals-page.component';
+import { SizingPageComponent } from './components/sizing-page/sizing-page.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent,
+    CustomerLoginComponent,
+    CustomerSignUpComponent,
+    HomePageComponent,
+    MakePaymentComponent,
+    NewArrivalsPageComponent,
+    SizingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
